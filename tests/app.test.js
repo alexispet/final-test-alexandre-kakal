7416bfedc0de0e84bb6783360a1b32e67fe7934e
alexandre-kakal-akarah <alexandre.kakal@akarah.com>
2024-01-19 09:28:32 +0100

    feat: local linter

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,13 @@
-import request from 'supertest';
-import app from '../app.js'
+import request from "supertest";
+import app from "../app.js";
+import { describe, it } from "mocha";
 
-describe('GET /post', function() {
-    it('responds with json', function(done) {
-        request(app)
-            .get('/post')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
-    });
+describe("GET /post", function () {
+	it("responds with json", function (done) {
+		request(app)
+			.get("/post")
+			.set("Accept", "application/json")
+			.expect("Content-Type", /json/)
+			.expect(200, done);
+	});
 });
